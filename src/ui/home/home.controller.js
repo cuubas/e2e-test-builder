fe5ac576eba52960fef374a9c3e45509bb03e061
Domas Trijonis <domas.trijonis@gmail.com>
2017-04-13 21:36:12 +0200

    refactored home controller to use ioproxy lib

--- src/ui/home/home.controller.js
+++ src/ui/home/home.controller.js
@@ -1,5 +1,6 @@
 var messenger = require('./../../common/messenger');
 var supportedFormats = require('./../../common/supported-formats');
+var ioproxy = require('./../../common/ioproxy');
 
 function HomeController($rootScope, $scope, $window) {
   var $ctrl = this, file, formatter;
@@ -9,73 +10,63 @@ function HomeController($rootScope, $scope, $window) {
   $ctrl.$onInit = function () {
     checkRecordingStatus();
 
+    // ioproxy.about().then(function (response) {
+    //   alert(response.version);
+    // });
+
     messenger.bind({
       recordingToggled: checkRecordingStatus
     });
 
     if ($window.localStorage.lastPath) {
-      $rootScope.pageTitle = $window.localStorage.lastPath;
-
-      chrome.runtime.sendNativeMessage('com.cuubas.ioproxy',
-        { op: "read", path: $window.localStorage.lastPath },
-        function (response) {
-          if (!handleError(response)) {
-            file = response;
-            formatter = supportedFormats.filter((f) => f.test(file.path))[0];
-            if (formatter) {
-              $ctrl.testCase = formatter.parse(response.data);
-              $scope.$digest();
-            } else {
-              alert('unsupported file format');
-            }
-          } else {
-            $window.localStorage.removeItem('lastPath');
-          }
-        });
-    }
+      $rootScope.pageTitle = '';
+      $ctrl.read($window.localStorage.lastPath);
+    };
   };
 
   $ctrl.toggleRecording = function () {
     messenger.send({ call: 'toggleRecording' });
-  }
+  };
+
+  $ctrl.read = function (path) {
+    ioproxy.read(path)
+      .then(processFile)
+      .catch(handleError);
+  };
 
   $ctrl.open = function () {
-    chrome.runtime.sendNativeMessage('com.cuubas.ioproxy',
-      { op: "open", lastPath: $window.localStorage.lastPath },
-      function (response) {
-        if (!handleError(response)) {
-          if (response.path) {
-            $rootScope.pageTitle = $window.localStorage.lastPath = response.path;
-          }
-          file = response;
-          formatter = supportedFormats.filter((f) => f.test(file.path))[0];
-          if (formatter) {
-            $ctrl.testCase = formatter.parse(response.data);
-            $scope.$apply();
-          } else {
-            alert('unsupported file format');
-          }
-        }
-      });
-  }
+    ioproxy.open($window.localStorage.lastPath)
+      .then(processFile)
+      .catch(handleError);
+  };
 
   $ctrl.save = function (ev, saveAs) {
-    if (!file || saveAs) {
-      file = { path: undefined }
-    }
-    chrome.runtime.sendNativeMessage('com.cuubas.ioproxy',
-      { op: "write", path: file.path, data: formatter.stringify($ctrl.testCase), lastPath: $window.localStorage.lastPath },
-      function (response) {
-        if (!handleError(response)) {
-          file = response;
-          if (response.path) {
-            $rootScope.pageTitle = $window.localStorage.lastPath = response.path;
-            $scope.$apply();
-          }
-          console.log(response);
+    ioproxy.write(!saveAs && file ? file.path : undefined, formatter.stringify($ctrl.testCase), $window.localStorage.lastPath)
+      .then((response) => {
+        file = response;
+        if (response.path) {
+          $rootScope.pageTitle = $window.localStorage.lastPath = response.path;
+          $scope.$apply();
         }
-      });
-  }
+      })
+      .catch(handleError);
+  };
+
+  $ctrl.$onInit();
+
+  function processFile(_file) {
+    file = _file;
+    if (file.path) {
+      $rootScope.pageTitle = $window.localStorage.lastPath = file.path;
+    }
+    formatter = supportedFormats.filter((f) => f.test(file.path))[0];
+    if (formatter) {
+      $ctrl.testCase = formatter.parse(file.data);
+      $scope.$apply();
+    } else {
+      throw new Error('unsupported file format');
+    }
+  };
 
   function checkRecordingStatus() {
     // get initial state
@@ -85,15 +76,9 @@ function HomeController($rootScope, $scope, $window) {
     });
   }
 
-  function handleError(response) {
-    var error = response && response.code < 1 ? response : chrome.runtime.lastError
-    if (error) {
-      alert(error.message || "an error occured");
-      return true;
-    }
-    return false;
+  function handleError(error) {
+    alert(error);
   }
-  $ctrl.$onInit();
 
 }
 
